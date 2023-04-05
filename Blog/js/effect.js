7d4783239ce212c324a5e09ec6ff56a49c2b790e
Tominne <108726281+Tominne@users.noreply.github.com>
2023-04-05 17:37:51 +1200

    Cleaned up files

---
+++ Blog/js/effect.js
@@ -0,0 +1,4 @@
+html = document.childNodes[1]
+body = html.childNodes[1]
+p = body.childNodes[1]
+p.text = 'Welcome to my study blog main page.'
--- effect.js
+++
@@ -1,4 +0,0 @@
-html = document.childNodes[1]
-body = html.childNodes[1]
-p = body.childNodes[1]
-p.text = 'Welcome to my study blog main page.'
