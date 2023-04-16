ba8aef3fbcb5b87208fcaed43713374ce4cad998
Min <min.elliot48@gmail.com>
2023-04-16 17:17:34 +1200

    tech js blog made and app linked

--- js/effect.js
+++ js/effect.js
@@ -2,3 +2,54 @@ html = document.childNodes[1]
 body = html.childNodes[1]
 p = body.childNodes[1]
 p.text = 'Welcome to my study blog main page.'
+
+person1 = {
+  age: 2,
+  name: "jeff",
+  last_name: "John"
+}
+
+document.getElementById("")
+
+
+
+
+x = "banana"
+
+if (x == "banana") {
+  console.log("banana")
+}
+else if (x == "apple") {
+  console.log("apple")
+}
+else {
+  console.log("idk")
+}
+
+
+switch (x) {
+  case "banana": 
+    console.log("banana")
+    break 
+  case "apple": 
+    console.log("apple")
+    break
+  default: console.log("idk")
+}
+
+// For Each
+
+pets = ["cute", "large", "fluffy", "Snuggly"]
+
+for (num in pets) {
+  console.log(pets[num])
+  if (pets[num] == "fluffy") {
+    break
+  }
+}
+
+/*
+lots 
+of 
+lines
+*/
---
+++ python/objects.py
@@ -0,0 +1,17 @@
+person1_age = 2
+person1_name = 'jeff'
+person1_last_name = 'John'
+
+
+person2_age = 5
+person2_name = 'bob'
+person2_last_name = 'james'
+
+
+person1 = {
+  "age": 2,
+  "name": "jeff",
+  "last_name": "John"
+}
+
+
