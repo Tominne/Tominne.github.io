cd8844ea49ea47354f76dfb8e2c1d8b6c41a6925
Min <min.elliot48@gmail.com>
2023-05-03 10:48:15 +1200

    clock in the maKING

--- games_/clockapp/clock.js
+++ games_/clockapp/clock.js
@@ -1,89 +1,82 @@
-var wakeupTime = 7;
-var noon = 12;
-var lunchTime = 12;
-var napTime = lunchtime + 2;
-var partyTime;
-var evening = 18;
+let wakeUpTime = 7;
+let noon = 12;
+let lunchTime = 12;
+let napTime = lunchTime + 2;
+let partyTime;
+let evening = 18;
+let messageText = document.getElementById('timeEventJS').innerHTML;
 
 //show time
 var showCurrentTime = function () {
-  var clock = document.getElementById("clock");
-  var currentTime = new Date();
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  
+  let formatHours = convertFormat(hours);
+  
+  hours = checkTime(hours);
+  
+  hours = addZero(hours);
+  minutes = addZero(minutes);
+  seconds = addZero(seconds);
+  
+  document.getElementById('clock').innerHTML = `${hours} : ${minutes} : ${seconds} ${formatHours}`
+}
 
-  var hours = currentTime.getHours();
-  var minutes = currentTime.getMinutes();
-  var seconds = currentTime.getSeconds();
-  var meridian = "AM";
-
-  //set hours
-
-  if (hours >= noon) {
-    meridian = "PM";
+function convertFormat(time) {
+  let format = 'AM';
+  if (time >= 12) {
+      format = 'PM';
   }
-  if (hours > noon) {
-    hours = hours - 12;
+  return format;
+}
+
+function checkTime(time) {
+  if (time > 12) {
+      time = time - 12;
   }
-
-  //set minutes
-  if (minutes < 10) {
-    minutes = "0" + minutes;
+  if (time === 0) {
+      time = 12;
   }
+  return time;
+}
 
-  //set seconds
-  if (minutes < 10) {
-    seconds = "0" + seconds;
+function addZero(time) {
+  if (time < 10) {
+      time = "0" + time;
   }
+  return time;
+}
 
-  //combine string and clock display
-  var clockTime = hours + ":" + minutes + seconds + " " + meridian + "!";
-
-  clock.innerText = clockTime;
-};
+showCurrentTime();
+setInterval(showCurrentTime, 1000);
 
 //make clock change alone
-var updateClock = function () {
+function updateClock() {
   var time = new Date().getHours();
-  var messageText;
-  var image =
-    "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
-
-  var timeEventJS = document.getElementById("timeEvent");
-  var lolcatImageJS = document.getElementById("lolcatImage");
-
   if (time == partyTime) {
-    image =
-      "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/partyTime.jpg";
-    messageText = "Let's party!";
-  } else if (time == wakeupTime) {
-    image =
-      "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
+    messageText = "May I have this Boogie?!";
+  } else if (time == wakeUpTime) {
     messageText = "Wake up!";
   } else if (time == napTime) {
-    image =
-      "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
     messageText = "Sleep tight!";
-  }  else if (time < noon)
-  {
-    image = "https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg";
+  } else if (time < noon) {
     messageText = "Good morning!";
   }
-  else if (time >= evening)
-  {
-    image = "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cat_sleep.jpg";
+  else if (time >= evening) {
     messageText = "Good evening!";
   }
-  else
-  {
-    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
+
+  else {
     messageText = "Good afternoon!";
   }
 
   console.log(messageText);
-  timeEventJS.innerText = messageText;
-  lolcatImageJS.src = image;
+  timeEventJS.innerHTML = messageText;
 
   showCurrentTime();
-};
+}
 updateClock();
 
 var oneSecond = 1000;
@@ -98,22 +91,18 @@ var partyEvent = function()
 {
     if (partyTime < 0) 
     {
-        partTime = new Date().getHours();
-        partyTimeButton.innerText = "Party Over!";
-        partyTimeButton.style.backgroundColor = 
+        partyTime = new Date().getHours();
+        partyTimeButton.innerText = "Cry party no more!";
         partyTime = -1;
-        partyTimeButton.innerText = "Party Time!";
-        partyTimeButton.style.backgroundColor = "#222";
-    }"#0A8DAB";
+        partyTimeButton.innerText = "Boogie with me may you?!";
+    }
 }
-    else {};
-
 
 partyButton.addEventListener("click", partyEvent);
 partyEvent(); 
 
 var wakeUpEvent = function() {
-    wakeupTime = wakeUpTimeSelector.value;
+    wakeUpTime = wakeUpTimeSelector.value;
 };
 
 wakeUpTimeSelector.addEventListener("change", wakeUpEvent); 
@@ -122,7 +111,7 @@ var lunchTimeSelector =  document.getElementById("lunchTimeSelector");
 
 var lunchEvent = function()
 {
-    lunchtime = lunchTimeSelector.value;
+    lunchTime = lunchTimeSelector.value;
 };
 
 lunchTimeSelector.addEventListener("change", lunchEvent);
@@ -133,7 +122,7 @@ var napTimeSelector =  document.getElementById("napTimeSelector");
 
 var napEvent = function()
 {
-    naptime = napTimeSelector.value;
+    napTime = napTimeSelector.value;
 };
 
 napTimeSelector.addEventListener("change", napEvent);
