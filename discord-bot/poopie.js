850580cef44b4bba122ca34a181cd1c6e068be53
Min <min.elliot48@gmail.com>
2023-05-03 18:23:45 +1200

    made a gpt bot

---
+++ discord-bot/poopie.js
@@ -0,0 +1,44 @@
+import dotenv from "dotenv";
+import { Client, GatewayIntentBits } from "discord.js";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+const openai = new OpenAIApi(
+  new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  })
+);
+
+client.on("messageCreate", async function (message) {
+  if (message.author.bot) return;
+
+  try {
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            "The following is a conversation with an AI assistant and mental health support buddy. The assistant is sassy, sarcastic, weird, nerdy, funny, helpful, creative, and witty.\n\nUser: Hello!\nAI:",
+        },
+        { role: "user", content: message.content },
+      ],
+    });
+
+    const content = response.data.choices[0].message;
+    return message.reply(content);
+  } catch (err) {
+    return message.reply("Poopsie Bot has errored whoopsie poopsie!.");
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
